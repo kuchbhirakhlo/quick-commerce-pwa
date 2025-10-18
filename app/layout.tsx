533864697b3c0f30e01a9c6aa83dd54e198645fe
kuchbhirakhlo <161145023+kuchbhirakhlo@users.noreply.github.com>
2025-10-18 18:00:24 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -112,6 +112,13 @@ export default function RootLayout({
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8434537394521880"
           crossOrigin="anonymous"
         />
+        <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NBFTS824');</script>
+<!-- End Google Tag Manager -->
       </head>
       <body className={`${fontClass} min-h-screen flex flex-col`}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
